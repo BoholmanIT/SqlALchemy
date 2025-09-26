b291147c01cc56a0fc8c869b35e0a1a6005f25bf
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2025-09-26 12:15:08 +0700

    learn

--- tutorial.py
+++ tutorial.py
@@ -12,3 +12,12 @@ class Author(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     books = relationship("Book", back_populates="author")
+
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    author = relationship("Author", back_populates="books")
+    
+Base.metadata.create_all(engine)
