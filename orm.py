e2f544b5fc028a8649acaa3649dd6abe4369be14
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2025-09-26 12:44:29 +0700

    using session for add

---
+++ orm.py
@@ -0,0 +1,49 @@
+from sqlalchemy import create_engine
+
+engine = create_engine("sqlite:///example.db", echo=True)
+
+from typing import List, Optional
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user_account"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+    
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+    
+class Address(Base):
+    __tablename__ = "address"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    
+    user: Mapped["User"] = relationship(back_populates="addresses")
+    
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+Base.metadata.create_all(engine)
+
+
+from sqlalchemy.orm import Session
+
+with Session(engine) as session:
+    spongebob = User(
+        name="spongebob",
+        fullname="Spongebob Squarepants",
+        addresses=[Address(email_address="spongebob@sqlaclhemy.org")]
+    )
+    session.add_all([spongebob])
+    
+    session.commit()
